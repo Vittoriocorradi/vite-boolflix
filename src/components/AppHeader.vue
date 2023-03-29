5af6581fe6f48c2871979634e4bfe7ff6305c672
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-29 20:47:18 +0200

    Milestone 4 - stilizzazione header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,10 +15,10 @@
         <h1>
             boolflix
         </h1>
-        <div>
-            <label for="name-search"></label>
-            <input type="text" id="name-search" placeholder="Inserire il titolo" @keyup="$emit('type')" v-model="store.searchInput">
-            <button type="button" @click="$emit('search')">Cerca</button>
+        <div class="search-bar">
+            <label for="name-search"><i class="fa-solid fa-magnifying-glass"></i></label>
+            <input type="text" id="name-search" placeholder="Inserire un titolo" @keyup="$emit('type')" v-model="store.searchInput">
+            <!-- <button type="button" @click="$emit('search')">Cerca</button> -->
         </div>
         
     </header>
@@ -33,15 +33,41 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding: 1.25rem 3.75rem;
+        padding: .625rem 1.875rem;
 
         h1 {
             color: var(--primary-color);
             text-transform: uppercase;
         }
 
-        input {
+        .search-bar {
+            border: .0625rem solid #fff;
+            color: var(--secondary-color);
+            width: 18.75rem;
+            display: flex;
+            align-items: center;
             
+            label {
+                padding: .75rem;
+                display: inline-block;
+                height: 100%;
+            }
+
+            input {
+                padding: .625rem 0;
+                background-color: inherit;
+                color: inherit;
+                border: none;
+                display: inline-block;
+                width: max-content;
+                flex-grow: 1;
+
+                &:focus {
+                    outline: none;
+                }
+            
+            
+            }
         }
     }
 </style>
